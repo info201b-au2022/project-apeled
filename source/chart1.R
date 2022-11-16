644ce4a7860e06209ebbda39c09e66bb73d8ae62
apeled <amitp.9787@gmail.com>
2022-11-16 15:40:11 -0800

    Summary table

---
+++ source/chart1.R
@@ -0,0 +1,40 @@
+library(dplyr)
+library(stringr)
+library(ggplot2)
+
+health_expenditure <- read.csv("https://raw.githubusercontent.com/info201b-au2022/project-apeled/main/data/CHE_data.csv")
+names(health_expenditure) <- health_expenditure[1,]
+health_expenditure = health_expenditure[-1,]
+health_expenditure <- health_expenditure[!is.na(health_expenditure$`2019`),]
+
+health_expenditure <- health_expenditure %>% 
+  mutate("Current health expenditure (CHE) per capita in US$" = health_expenditure$`2019`) %>% 
+  select(Country, `Current health expenditure (CHE) per capita in US$`)
+
+hiv <- read.csv("https://raw.githubusercontent.com/info201b-au2022/project-apeled/main/data/prev_hiv.csv")
+names(hiv) <- hiv[1,]  
+hiv = hiv[-1,]
+
+hiv <- hiv[hiv$` 2019` != "No data",]
+hiv[hiv=="&lt;0.1 [&lt;0.1-0.2]"] <- "0.1"
+hiv[hiv=="&lt;0.1 [&lt;0.1-&lt;0.1]"] <- "0.1"
+hiv[hiv=="&lt;0.1 [&lt;0.1-0.1]"] <- "0.1"
+hiv[hiv=="&lt;0.1 [&lt;0.1-0.3]"] <- "0.1"
+
+hiv <- hiv %>% 
+  mutate("Prevalence of HIV among adults aged 15 to 49 (%)" = str_remove(hiv$` 2019`, "\\[.*")) %>% 
+  select(Country, `Prevalence of HIV among adults aged 15 to 49 (%)`)
+
+hiv$`Prevalence of HIV among adults aged 15 to 49 (%)` <- as.numeric(hiv$`Prevalence of HIV among adults aged 15 to 49 (%)`)
+
+merged_data <- inner_join(hiv, health_expenditure, by = "Country")
+
+graph2 <- ggplot(data = merged_data) +
+  geom_dotplot(mapping =  aes(x = `Prevalence of HIV among adults aged 15 to 49 (%)`, 
+<<<<<<< HEAD
+                              y = `Current health expenditure (CHE) per capita in US$`,
+                              color = Country))
+=======
+                            y = `Current health expenditure (CHE) per capita in US$`,
+                            color = Country))
+>>>>>>> 475963416b2335a7dd1ebc8dd81a4fc90aa6685f
---
+++ source/chart3.R
@@ -0,0 +1,57 @@
+library("tidyr")
+library("dplyr")
+library("ggplot2")
+library("stringr")
+
+Med_data <- read.csv("https://raw.githubusercontent.com/info201b-au2022/project-apeled/main/data/med_aval.csv")
+HIV_data <- read.csv("https://raw.githubusercontent.com/info201b-au2022/project-apeled/main/data/prev_hiv.csv")
+
+names(HIV_data) <- HIV_data[1,]
+HIV_data = HIV_data[-1,]
+
+HIV_data <- HIV_data[HIV_data$` 2021` != "No data",]
+
+names(Med_data) <- Med_data[1,]
+Med_data = Med_data[-1,]
+Med_data = Med_data[-3]
+
+colnames(Med_data)[2] = "Median Availability of Generic Medicines 2007-2013 (%)"
+
+HIV_data[HIV_data=="&lt;0.1 [&lt;0.1-0.2]"] <- "0.1"
+HIV_data[HIV_data=="&lt;0.1 [&lt;0.1-&lt;0.1]"] <- "0.1"
+HIV_data[HIV_data=="&lt;0.1 [&lt;0.1-0.1]"] <- "0.1"
+HIV_data[HIV_data=="&lt;0.1 [&lt;0.1-0.3]"] <- "0.1"
+
+HIV_data <- HIV_data %>%
+  mutate(data_2013 = str_remove(HIV_data$` 2013`, "\\[.*"))
+HIV_data <- HIV_data %>%
+  mutate(data_2012 = str_remove(HIV_data$` 2012`, "\\[.*"))
+HIV_data <- HIV_data %>%
+  mutate(data_2011 = str_remove(HIV_data$` 2011`, "\\[.*"))
+HIV_data <- HIV_data %>%
+  mutate(data_2010 = str_remove(HIV_data$` 2010`, "\\[.*"))
+HIV_data <- HIV_data %>%
+  mutate(data_2009 = str_remove(HIV_data$` 2009`, "\\[.*"))
+HIV_data <- HIV_data %>%
+  mutate(data_2008 = str_remove(HIV_data$` 2008`, "\\[.*"))
+HIV_data <- HIV_data %>%
+  mutate(data_2007 = str_remove(HIV_data$` 2007`, "\\[.*"))
+
+HIV_data$data_2013 <- as.numeric(HIV_data$data_2013)
+HIV_data$data_2012 <- as.numeric(HIV_data$data_2012)
+HIV_data$data_2011 <- as.numeric(HIV_data$data_2011)
+HIV_data$data_2010 <- as.numeric(HIV_data$data_2010)
+HIV_data$data_2009 <- as.numeric(HIV_data$data_2009)
+HIV_data$data_2008 <- as.numeric(HIV_data$data_2008)
+HIV_data$data_2007 <- as.numeric(HIV_data$data_2007)
+
+HIV_data <- HIV_data %>%
+  mutate("Median Prevalence of HIV 2007-2013 (%)" = rowMeans(HIV_data[,24:30], na.rm = TRUE))
+
+merged_data <- inner_join(HIV_data, Med_data, by = "Country")
+
+Graph3 <- ggplot(data = merged_data) +
+  geom_point(mapping =  aes(x = `Median Prevalence of HIV 2007-2013 (%)`, y = `Median Availability of Generic Medicines 2007-2013 (%)`, color = Country))
+print(Graph3)
+
+#
