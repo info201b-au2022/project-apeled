aa0f69cfd397e65216f77a3f08cc627898a7051b
apeled <amitp.9787@gmail.com>
2022-12-07 20:32:32 -0800

    Amit: server&chart2 variable names

--- source/shinyApp/tabs/tab_panel_chart2.R
+++ source/shinyApp/tabs/tab_panel_chart2.R
@@ -8,13 +8,8 @@ tab_panel_chart2 <-tabPanel(
   h3("Comparing Median Availability of Generic Medicines to Prevalence of HIV"),
   sidebarLayout(
     sidebarPanel(
-<<<<<<< HEAD
-      sliderInput("country", label = h3("Number of Countries"), min = 1, step = 1, 
-                  max = nrow(merged_data_chart2), value = nrow(merged_data_chart2))
-=======
       sliderInput("country", label = h3("How Many Countries Would You Like to See?"), min = 1, step = 1, 
-                  max = nrow(merged_data), value = nrow(merged_data))
->>>>>>> a67c627a8c10429fcd46815f0b0b4f3955b54d86
+                  max = nrow(merged_data_chart2), value = nrow(merged_data_chart2))
     ),
     mainPanel(
       plotOutput(outputId = "chart2")
